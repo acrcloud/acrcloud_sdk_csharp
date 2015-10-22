334904d1980ec43e0d4967f86ace88932319fddf
pony <pony@ponydeMacBook-Pro.local>
2015-10-22 19:29:36 +0800

    add acrcloud_extr_tool.h

---
+++ libs/win32/dll_acr_extr_tool.h
@@ -0,0 +1,26 @@
+#ifndef _Included_DLL_acr_extr_tool_H
+#define _Included_DLL_acr_extr_tool_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef ACRCLOUDEXTR_EXPORTS
+#define ACRCLOUDEXTR_API __declspec(dllexport)
+#else
+#define ACRCLOUDEXTR_API __declspec(dllimport)
+#endif
+
+ACRCLOUDEXTR_API int __stdcall create_fingerprint(char *pcm_buffer, int pcm_buffer_len, char is_db_fingerprint, char **fps_buffer);
+ACRCLOUDEXTR_API int __stdcall create_fingerprint_by_file(char *file_path, int start_time_seconds, int audio_len_seconds, char is_db_fingerprint, char **fps_buffer);
+ACRCLOUDEXTR_API int __stdcall create_fingerprint_by_filebuffer(char *file_buffer, int file_buffer_len, int start_time_seconds, int audio_len_seconds, char is_db_fingerprint, char **fps_buffer);
+ACRCLOUDEXTR_API int __stdcall decode_audio_by_file(char *file_path, int start_time_seconds, int audio_len_seconds, char **audio_buffer);
+ACRCLOUDEXTR_API int __stdcall decode_audio_by_filebuffer(char *file_buffer, int file_buffer_len, int start_time_seconds, int audio_len_seconds, char **audio_buffer);
+ACRCLOUDEXTR_API void __stdcall acr_free(char *buffer);
+ACRCLOUDEXTR_API void __stdcall acr_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

aa2c4edc5229b9451c82608f90f816aa6993ef86
qinxue.pan <xue@acrcloud.com>
2017-11-01 18:17:28 +0800

    modify error code

--- libs/recognizer.cs
+++ libs/recognizer.cs
@@ -400,7 +400,7 @@ namespace ACRCloudRecognitionTest
         public static string HTTP_ERROR = "{\"status\":{\"msg\":\"Http Error\", \"code\":3000}}";
         public static string NO_RESULT = "{\"status\":{\"msg\":\"No Result\", \"code\":1001}}";
         public static string GEN_FP_ERROR = "{\"status\":{\"msg\":\"Gen Fingerprint Error\", \"code\":2004}}";
-        public static string DECODE_AUDIO_ERROR = "{\"status\":{\"msg\":\"Can not decode audio data\", \"code\":2005}}";
+        public static string DECODE_AUDIO_ERROR = "{\"status\":{\"msg\":\"Can not decode audio data\", \"code\":2004}}";
         public static string RECORD_ERROR = "{\"status\":{\"msg\":\"Record Error\", \"code\":2000}}";
         public static string JSON_ERROR = "{\"status\":{\"msg\":\"json error\", \"code\":2002}}";
     }
